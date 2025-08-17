f09533c010b8110fc060f1d766f3faca04c9e07e
Jhoseph <jhoseph@Jhoseph.Jhoseph>
2025-08-16 21:50:19 -0400

    add model

---
+++ src/main/java/com/example/producto/model/Producto.java
@@ -0,0 +1,36 @@
+package com.example.producto.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String precio;
+
+    @Column(nullable = false)
+    private String stock;
+}
